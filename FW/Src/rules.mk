78f5759264d7cb86b34157d0da9087c68ee39db2
arash <arash.golgol@gmail.com>
2021-05-08 21:00:15 +0430

    Adding lwrb to be used as FIFO for data streaming

--- FW/Src/rules.mk
+++ FW/Src/rules.mk
@@ -11,18 +11,32 @@ Src/LCDConf.c \
 Src/misc_fatfs.c \
 Src/retarget.c \
 Src/vs10xx.c \
-Src/mp3_player.c 
+Src/mp3_player.c \
+Src/ili9486.c \
+Middlewares/Third_Party/lwrb/lwrb/src/lwrb/lwrb.c
 
 
 ######################################
 # C includes
 ######################################
 C_INCLUDES +=  \
--IMiddlewares/Third_Party/STemWin/inc
-
+-IMiddlewares/Third_Party/STemWin/inc \
+-IMiddlewares/Third_Party/lwrb/lwrb/src/include
 
 #######################################
 # LDFLAGS
 ####################################### 
 LIBS += -lSTemWin_CM3_wc16
 LIBDIR += -LMiddlewares/Third_Party/STemWin 
+
+#######################################
+# C defines
+#######################################
+C_DEFS += -DLCD_ILI9340
+
+#######################################
+# C Options
+#######################################
+OPT += -fshort-wchar
+
+# *** EOF ***
